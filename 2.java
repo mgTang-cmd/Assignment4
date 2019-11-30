16b5ead615e1b1d6c3d2aa1a1538a536ba43c906
mgTang-cmd <58256108+mgTang-cmd@users.noreply.github.com>
2019-11-30 19:10:45 +0800

    Update 2.java

--- 2.java
+++ 2.java
@@ -77,6 +77,4 @@ throws Exception {
     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 }
-————————————————
-版权声明：本文为CSDN博主「zmycoco2」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
-原文链接：https://blog.csdn.net/michaelzhou224/article/details/10150387
+
